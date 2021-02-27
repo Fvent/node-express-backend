ab684277ce9aa7516aadc7dd96e08bfb70cffb35
Francis <francisventura1@hotmail.com>
2021-02-27 17:31:39 -0500

    adding some encryption

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var forge = require('node-forge');
 
+var dotenv = require('dotenv');
+dotenv.config();
+
+var privateKey = forge.pki.privateKeyFromPem(process.env.PRIVATE_KEY);
 
 const PORT = 1515;
+const NVR = "Error occured on server";
 
 var app = express();
 
@@ -82,25 +88,27 @@ app.post('/addcomment', function(req,res){
     }
 });
 
+app.post('/adduser', (req,res) => {
+    var userData = req.body;
+    var newUser = new User({
+        name: userData.name,
+        alias: userData.alias,
+        password: privateKey.decrypt(userData.password)
+    });
+    newUser.save().then(res.send('ok'));
+});
+
 app.post('/login',function(req,res){
     var requestedUser = req.body;
     console.log(requestedUser);
-    // User.find({name: requestedUser.name, password: requestedUser.password}, 
-    //     (err, response) => {
-    //         if(err){
-    //             console.log(err);
-    //             res.send('error on server');
-    //         }else{
-    //             console.log(response);
-    //             res.send({name: response.name, alias: response.alias});
-    //         }
-    //     });
+    
     User.find({'name': requestedUser.name, 'password': requestedUser.password}).select('name alias').exec((err ,response) => {
         try{
-            console.log(response);
+            console.log(JSON.stringify(response));
+            // res.send(JSON.stringify(response));
             res.send(response);
         }catch(err){
-            res.send('error occured');
+            res.send(NVR);
         }
     });
     
